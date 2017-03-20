8f13e8df7c41230eb0a1a5055e5fcdb84a143361
Rafael Takasu <rafaeltakasu@Rafaels-MacBook-Pro-2.local>
2017-03-20 15:43:36 -0400

    commented

--- app/src/main/java/com/itorras/project1/MainActivity.java
+++ app/src/main/java/com/itorras/project1/MainActivity.java
@@ -8,11 +8,10 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.view.View;
-import java.util.ArrayDeque;
-import java.util.Queue;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Declare imageviews and buttons
     private ImageView playerView, cmpView;
     private ImageView pastCmpView1;
     private ImageView pastCmpView2;
@@ -20,10 +19,12 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton e_imgBtn, m_imgBtn, c_imgBtn;
     private Button randomBtn;
     private TextView result_tv, count_tv, win_tv, comp_win_tv;
+
     int count = 0;
     int playerWinCount = 0;
     int compWinCount = 0;
-    int lastAnimal = 0;
+
+    // array to keep track of last animals played by computer
     int[] lastAnimals = new int[3];
 
     @Override
@@ -31,12 +32,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         //initialize buttons
         e_imgBtn = (ImageButton) findViewById(R.id.btnElephant);
         c_imgBtn = (ImageButton) findViewById(R.id.btnCat);
         m_imgBtn = (ImageButton) findViewById(R.id.btnMouse);
-
         randomBtn = (Button) findViewById(R.id.random);
 
         //intialize Image Views
@@ -51,18 +50,20 @@ public class MainActivity extends AppCompatActivity {
         win_tv = (TextView) findViewById(R.id.winCount);
         comp_win_tv = (TextView) findViewById(R.id.compWinCount);
 
+        // onClickListener for the player's buttons
         MyOnClickListener myOnClickListener = new MyOnClickListener();
         e_imgBtn.setOnClickListener(myOnClickListener);
         c_imgBtn.setOnClickListener(myOnClickListener);
         m_imgBtn.setOnClickListener(myOnClickListener);
 
-
+        // onClickListener for the random button
         RandomOnClickListener randomOnClickListener = new RandomOnClickListener();
         randomBtn.setOnClickListener(randomOnClickListener);
 
     }
 
     public void setImage(ImageView imgView, int animal) {
+        // method takes in an imageView object and sets an animal to it
 
         switch( animal ) {
             case 1:
@@ -83,59 +84,61 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
 
+            // Display the last three animals played by the computer
             setImage(pastCmpView1, lastAnimals[0]);
             setImage(pastCmpView2, lastAnimals[1]);
             setImage(pastCmpView3, lastAnimals[2]);
 
-            int rand = (int) (Math.random() * 3 + 1); // get a random numbe form 1 to 3
+            // Pick a random number from 1-3 that represents the computer's choice
+            int rand = (int) (Math.random() * 3 + 1);
+            setImage(cmpView, rand);
 
+            // Update the array storing the last 3 animals played by the computer
             for (int i = 2; i > 0; i--){
                 lastAnimals[i] = lastAnimals[i-1];
             }
             lastAnimals[0] = rand;
 
             count++;//
-            switch (rand) { /*** rand = 1 means computer is Elephant, * 2 represents Mouse,* 3 represents Cat*/
+            switch (rand) { // rand = 1 means computer chose elephant, 2 represents mouse,3 represents cat
                 case 1:
-                    cmpView.setImageResource(R.drawable.elephant); //computer choose Elephant
                     switch (v.getId()) {
-                        case R.id.btnElephant: //player choose Elephant
-                            playerView.setImageResource(R.drawable.elephant);
+                        case R.id.btnElephant: //both chose elephant, so it's a tie
+                            setImage(playerView, 1);
                             result_tv.setText("Result: " + "Tied!");
                             count_tv.setText("Round: " + count);
                             break;
-                        case R.id.btnMouse: //player choose Mouse
-                            playerView.setImageResource(R.drawable.mouse);
+                        case R.id.btnMouse: //player chose mouse and computer chose elephant, so player wins
+                            setImage(playerView, 2);
                             result_tv.setText("Result: " + "Win!");
                             count_tv.setText("Round: " + count);
-                            playerWinCount++;
+                            playerWinCount++; // increase player win count
                             win_tv.setText("Player Win Count: " + playerWinCount);
                             break;
-                        case R.id.btnCat: //player choose Cat
-                            playerView.setImageResource(R.drawable.cat);
+                        case R.id.btnCat: //player chose cat and computer chose elephant, so computer wins
+                            setImage(playerView, 3);
                             result_tv.setText("Result: " + "Lose!");
                             count_tv.setText("Round: " + count);
-                            compWinCount++;
+                            compWinCount++; // increase computer win count
                             comp_win_tv.setText("Computer Win Count: " + compWinCount);
                             break; }
                     break;
                 case 2:
-                    cmpView.setImageResource(R.drawable.mouse); //computer choose Mouse
                     switch (v.getId()) {
                         case R.id.btnElephant:
-                            playerView.setImageResource(R.drawable.elephant);
+                            setImage(playerView, 1);
                             result_tv.setText("Result: " + "Lose!");
                             count_tv.setText("Round: " + count);
                             compWinCount++;
                             comp_win_tv.setText("Computer Win Count: " + compWinCount);
                             break;
                         case R.id.btnMouse:
-                            playerView.setImageResource(R.drawable.mouse);
+                            setImage(playerView, 2);
                             result_tv.setText("Result: " + "Tie!");
                             count_tv.setText("Round: " + count);
                             break;
                         case R.id.btnCat:
-                            playerView.setImageResource(R.drawable.cat);
+                            setImage(playerView, 3);
                             result_tv.setText("Result: " + "Win!");
                             count_tv.setText("Round: " + count);
                             playerWinCount++;
@@ -143,24 +146,23 @@ public class MainActivity extends AppCompatActivity {
                             break; }
                     break;
                 case 3:
-                    cmpView.setImageResource(R.drawable.cat); //computer chose Cat
                     switch (v.getId()) {
                         case R.id.btnElephant:
-                            playerView.setImageResource(R.drawable.elephant);
+                            setImage(playerView, 1);
                             result_tv.setText("Result: " + "Win!");
                             count_tv.setText("Round: " + count);
                             playerWinCount++;
                             win_tv.setText("Player Win Count: " + playerWinCount);
                             break;
                         case R.id.btnMouse:
-                            playerView.setImageResource(R.drawable.mouse);
+                            setImage(playerView, 2);
                             result_tv.setText("Result: " + "Lose!");
                             count_tv.setText("Round: " + count);
                             compWinCount++;
                             comp_win_tv.setText("Computer Win Count: " + compWinCount);
                             break;
                         case R.id.btnCat:
-                            playerView.setImageResource(R.drawable.cat);
+                            setImage(playerView, 3);
                             result_tv.setText("Result: " + "Tie!");
                             count_tv.setText("Round: " + count);
                             break; }
@@ -170,6 +172,10 @@ public class MainActivity extends AppCompatActivity {
         private class RandomOnClickListener implements View.OnClickListener {
             @Override
             public void onClick(View v) {
+                // When the random button is clicked, a random int is chosen from 1-3 to represent
+                // the a random choice for the player. 1=elephant, 2=mouse, 3=cat.
+                // This then simulates a button click for the animal that was picked.
+
                 int rand = (int) (Math.random() * 3 + 1);
 
                 switch (rand) {
